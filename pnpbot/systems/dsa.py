ac29e820c9f6be1bbd41c937856778398ecde251
Alexander Zeindlhofer <zeindy@Alexanders-MBP.fritz.box>
2020-04-04 13:34:32 +0200

    reformated code

--- pnpbot/systems/dsa.py
+++ pnpbot/systems/dsa.py
@@ -4,16 +4,32 @@ from discord.ext.commands import Context
 from .base import BaseSystem, Dice
 
 import logging
+
 _logger = logging.getLogger("pnpbot")
 
 
 class System(BaseSystem):
     Name = "DSA"
-    Attributes = ["MU", "KL", "IN", "CH", "FF", "GE", "KO", "KK", "LeP", "Aus", "AsP", "KaP"]
+    Attributes = [
+        "MU",
+        "KL",
+        "IN",
+        "CH",
+        "FF",
+        "GE",
+        "KO",
+        "KK",
+        "LeP",
+        "Aus",
+        "AsP",
+        "KaP",
+    ]
     RollArgs = [Dice, int, int, int, int]
     RollHelp = "Verwendung: !roll XdY BasisWert BasisWert BasisWert TalentWert  (z.B. `!roll 3d20 10 11 12 5`)"
 
-    async def handle_roll(self, ctx: Context, dice: Dice, base1: int, base2: int, base3: int, talent: int):
+    async def handle_roll(
+        self, ctx: Context, dice: Dice, base1: int, base2: int, base3: int, talent: int
+    ):
         if dice.number <= 0 or dice.sides <= 0:
             await ctx.send("Verwendung: !roll XdY Basis (e.g. `!roll 3d20 15`")
             return
@@ -25,45 +41,43 @@ class System(BaseSystem):
         if dice.sides > 100:
             await ctx.send("Die Würfel können nicht mehr als 100 Seiten haben.")
             return
- 
+
         base = [base1, base2, base3]
-        #_logger.debug(f"Base1: '{base[0]}' Base2: '{base[1]}' Base3: '{base[2]}'")  
-        
+        # _logger.debug(f"Base1: '{base[0]}' Base2: '{base[1]}' Base3: '{base[2]}'")
+
         results = [random.randint(1, dice.sides) for r in range(dice.number)]
 
-        #successes = sum([1 for r in results if r <= base[0]])
-        #_logger.debug(f"successes: '{successes}'")
+        # successes = sum([1 for r in results if r <= base[0]])
+        # _logger.debug(f"successes: '{successes}'")
         TaW = talent
-        successes = 0 
+        successes = 0
         results_out = []
 
         _logger.debug(f"talent: '{TaW}'")
-        i=-1 
+        i = -1
         for r in results:
             i += 1
-            if r<=base[i]:
+            if r <= base[i]:
                 successes += 1
                 results_out.append(f"**{r}**")
             else:
-                if r<=base[i]+TaW:
-                    successes +=1
+                if r <= base[i] + TaW:
+                    successes += 1
                     results_out.append(f"**{r}**")
-                    TaW -= r-base[i]
+                    TaW -= r - base[i]
                 else:
                     results_out.append(str(r))
-                    TaW -= r-base[i]
-
-
-
-        #_logger.debug(f"talent: '{TaW}'")
-        #_logger.debug(f"successes: '{successes}'") 
+                    TaW -= r - base[i]
+        # _logger.debug(f"talent: '{TaW}'")
+        # _logger.debug(f"successes: '{successes}'")
         success = successes >= 3
-        i=0
+        i = 0
         for r in results:
-                if r == 1:
-                    i+=1
-        if i>=2: success = 3
-        #_logger.debug(f"success: '{success}'") 
+            if r == 1:
+                i += 1
+        if i >= 2:
+            success = 3
+        # _logger.debug(f"success: '{success}'")
 
         msg = ""
         KritErf = 0
@@ -72,7 +86,7 @@ class System(BaseSystem):
         if success:
             for r in results:
                 if r == 1:
-                    KritErf+=1
+                    KritErf += 1
             if KritErf >= 2:
                 msg = (
                     f":fire: :fire: :fire: **Kritischer Erfolg!** :fire: :fire: :fire:"
@@ -82,7 +96,7 @@ class System(BaseSystem):
         else:
             for r in results:
                 if r == 20:
-                    KritMisErf+=1
+                    KritMisErf += 1
 
             if KritMisErf >= 2:
                 msg = f":zap: :zap: :zap: **Kritischer Misserfolg!** :zap: :zap: :zap:"
